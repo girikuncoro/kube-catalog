e720d611189bd36fa5afb14b1fab06d0ea2e1809
Linda Luo <lanl1@vmware.com>
2017-07-20 16:25:16 -0700

    modified status.json to be consistant with BE. modified logic to display url and credentials.

--- js/status.js
+++ js/status.js
@@ -3,13 +3,12 @@ const NUM_COLS = 5;
 
 getServiceData();
 
-
 function getServiceData() {
 	$.getJSON(services_status, (res) => {
 		console.log(res);
 		services = [];
-		for (i = 0; i < res.results.length; i++) {
-			services.push(res.results[i]);
+		for (i = 0; i < res.data.length; i++) {
+			services.push(res.data[i]);
 		}
 		num_rows = services.length;
 		num_cols = NUM_COLS;
@@ -20,7 +19,6 @@ function getServiceData() {
 
 
 function populateTable(services, num_rows, num_cols) {
-	debugger;
 	table = document.getElementById("statusTable");
 	for (var i = 0; i < num_rows; i++) {
 		row = table.insertRow(i);
@@ -29,18 +27,24 @@ function populateTable(services, num_rows, num_cols) {
 		cell0 = row.insertCell(j++);
 	    cell0.innerHTML= services[i].name;
 
-	    data = services[i].data;
+	    data = services[i];
+	    status = data.status;
 	    cell1 = row.insertCell(j++);
 	    cell1.innerHTML= data.bundle;
 
 	    cell2 = row.insertCell(j++);
-	    cell2.innerHTML= data.access_url;
-
 	    cell3 = row.insertCell(j++);
-	    cell3.innerHTML= data.credential;
+	    cell2.innerHTML= "unavailable";
+	    cell3.innerHTML= "unavailable";
+
+	    // access_url and credential would be available only when status is running
+	    if (status == "running") {
+	    	cell2.innerHTML= data.access_url;
+	    	cell3.innerHTML= data.credential;
+	    }
 
 	    cell4 = row.insertCell(j++);
-	    cell4.innerHTML= data.status;
+	    cell4.innerHTML= status;
 	}
 }
 
